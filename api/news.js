6bda7db36920da5bc70edbf20ed01200293892dd
soun190-design <soun190@gmail.com>
2026-04-28 16:38:15 +0900

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,8 +1,9 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  const category = req.query.category || 'general';
   try {
     const response = await fetch(
-      'https://gnews.io/api/v4/top-headlines?lang=ko&country=kr&max=4&apikey=bcf3dada057318a80aa25e747fdb5881'
+      'https://gnews.io/api/v4/top-headlines?lang=ko&country=kr&max=4&topic=' + category + '&apikey=bcf3dada057318a80aa25e747fdb5881'
     );
     const data = await response.json();
     res.status(200).json(data);
